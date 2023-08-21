34e5b2bd21c36585ca5240477d4105424ea40d20
kadir <kadiry202020@gmail.com>
2023-08-21 17:42:41 +0300

    rastgele gönderdim

---
+++ force-app/main/default/lwc/mustafaHoca/mustafaHoca.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+
+export default class MustafaHoca extends LightningElement {
+    sjxdhıxkdjzdkfhjgv
+    fgb
+    zfdgh
+    zhb
+    zfdghdzgfhz
+    dth
+    zfdghdzgfhzdzgh
+    zhbf
+    hfz
+    xgh
+    fzh
+    fzxh
+    z
+    h
+}
